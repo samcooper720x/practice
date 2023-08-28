7d9e4d3cf89da015b44b031c78d547a2bd94fc9d
Sam Cooper <003.sjc@gmail.com>
2023-08-28 21:34:50 +0100

    little-schemer/cons the magnificent pt2

--- the-little-schemer/cons-the-magnificent.scm
+++ the-little-schemer/cons-the-magnificent.scm
@@ -34,6 +34,18 @@
 
 (subst 'topping 'fudge '(ice cream with fudge for desert))
 
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond
+      ((null? lat) '())
+      (else
+        (cond
+          ((or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat)))
+          (else 
+            (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))))
+
+(subst2 'vanilla 'chocolate 'banana '(banana ice cream with chocolate topping))
+
 ; Define helpers
 (define atom?
  (lambda (x)
