73efcb91b5e00abe613c18695232ea30e912d7cb
Sam Cooper <003.sjc@gmail.com>
2023-08-31 20:48:43 +0100

    little-schemer/cons the magnificent pt3

--- the-little-schemer/cons-the-magnificent.scm
+++ the-little-schemer/cons-the-magnificent.scm
@@ -1,3 +1,13 @@
+(define rember
+  (lambda (a lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) a) (cdr lat))
+      (else
+        (cons (car lat) (rember a (cdr lat)))))))
+
+(rember 'and '(bacon lettuce tomato))
+
 (define insertR
   (lambda (new old lat)
     (cond
@@ -45,6 +55,46 @@
             (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))))
 
 (subst2 'vanilla 'chocolate 'banana '(banana ice cream with chocolate topping))
+
+(define multirember
+  (lambda (a lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) a) (multirember a (cdr lat)))
+      (else
+        (cons (car lat) (multirember a (cdr lat)))))))
+
+(multirember 'cup '(coffee cup tea cup and hick cup))
+
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old) (cons old (cons new (multiinsertR new old (cdr lat)))))
+      (else
+        (cons (car lat) (multiinsertR new old (cdr lat)))))))
+
+(multiinsertR 'why 'hello '(hello fred hello ed hello ted))
+
+(define multiinsertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old) (cons new (cons old (multiinsertL new old (cdr lat)))))
+      (else
+        (cons (car lat) (multiinsertL new old (cdr lat)))))))
+
+(multiinsertL 'fried 'fish '(chips and fish or fish and fried))
+
+(define multisubst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old) (cons new (multisubst new old (cdr lat))))
+      (else
+        (cons (car lat) (multisubst new old (cdr lat)))))))
+
+(multisubst 'goodbye 'hello '(hello fred hello ed hello ted))
 
 ; Define helpers
 (define atom?
